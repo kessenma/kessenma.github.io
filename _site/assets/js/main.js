eea54a7ceaf3d2e953209cb0dedeee165e2fc09e
kessenma <kessenmacher7832@gmail.com>
2021-10-28 19:27:05 -0400

    took out menu bar while working on it, fixed the diagonal clicking issue on the bottom

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -201,3 +201,22 @@ jQuery(document).ready(function(){
     });
   }
 });
+
+const angleRange = document.getElementById('angle-control');
+const angleResult = document.getElementById('angle-result');
+const root = document.querySelector('HTML');
+const boxes = document.querySelectorAll('.box');
+
+angleRange.addEventListener("input", function() {
+  const angle = angleRange.value;
+  const angleRad = angle * Math.PI / 180;
+  const magicNumber = Math.abs( Math.tan(angleRad) / 2 );
+  angleResult.innerHTML =  angleRange.value + " deg";
+  document.documentElement.style.setProperty('--angle', angleRange.value + 'deg');
+  document.documentElement.style.setProperty('--magic-number', magicNumber);
+  
+  // Sadly the animation only updates correctly in Firefox :-(
+  boxes.forEach((box) => {
+    box.style.animation = "none";
+  });
+}, false); 
