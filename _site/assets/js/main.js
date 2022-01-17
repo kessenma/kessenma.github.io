d2d55b4cdbc28d7c28d3494bf14447cd661b57af
Kyle Essenmacher <kessenmacher7832@gmail.com>
2022-01-17 01:11:20 -0500

    getting this fucking button to work if it kills me

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,4 +1,29 @@
 // #######################################################
+// ##################ANIMAATE ON SCROLL ##################
+// #######################################################
+AOS.init({
+  duration: 1200,
+  easing: 'ease-in-out-back'
+});
+
+// #######################################################
+// ################## Back button #########################
+// #######################################################
+
+var backButton = document.querySelector('.back-button')
+
+function backAnim(){
+  if (backButton.classList.contains('back')){
+    backButton.classList.remove('back');
+  }
+  else{
+    backButton.classList.add('back');
+    setTimeout(backAnim, 1000);
+  }
+}
+backButton.addEventListener('click', backAnim);
+
+// #######################################################
 // ################## MODAL SETUP #########################
 // #######################################################
 
@@ -194,70 +219,7 @@ var Modal = (function() {
 
 Modal.init();
 
-// #######################################################
-// ##################ANIMAATE ON SCROLL ##################
-// #######################################################
-AOS.init({
-  duration: 1200,
-  easing: 'ease-in-out-back'
-});
 
-var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 3500;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
-
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
-
-        if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-        var that = this;
-        var delta = 200 - Math.random() * 100;
-
-        if (this.isDeleting) { delta /= 2; }
-
-        if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-        }
-
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
-
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        document.body.appendChild(css);
-    };
 
 // https://github.com/tsuyoshiwada/sweet-scroll/blob/master/README.md#1-install
  document.addEventListener(
