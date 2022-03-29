d713d2d327ee05a97149c6b9a9ac35e657eb3fb7
Kessenma <kessenmacher7832@gmail.com>
2022-03-28 20:52:59 -0400

    added in favicon and reformatted text so hopefully it works on mobile

---
+++ _site/assets/js/main.js
@@ -0,0 +1,224 @@
+// #######################################################
+// ##################ANIMATE ON SCROLL ##################
+// #######################################################
+// AOS.init({
+//   duration: 1200,
+//   easing: 'ease-in-out-back'
+// });
+
+
+// #######################################################
+// ##################    particles ##################
+// #######################################################
+
+
+
+document.addEventListener("DOMContentLoaded",function(){new SweetScroll({});particlesJS("particles-js",{
+  "particles": {
+    "number": {
+      "value": 10,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.6,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 28.05962670675185,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 1.60340724038582,
+      "direction": "bottom",
+      "random": false,
+      "straight": true,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 400.851810096455,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "window",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 0,
+        "line_linked": {
+          "opacity": 0
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 1.118881118881119,
+        "opacity": 0.3516483516483517,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+})},!1);
+
+// #######################################################
+// ##################  https://codepen.io/sharkcoder/pen/WNedoNQ  #########################
+// ################## SciCrunch slide show
+// #######################################################
+
+jQuery(document).ready(function($) {
+  "use strict";
+  $('#customers-testimonials').owlCarousel( {
+      loop: false,
+      center: true,
+      items: 3,
+      margin: 30,
+      autoplay: false,
+      dots:true,
+      nav:true,
+      autoplayTimeout: 8500,
+      smartSpeed: 1200,
+      navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+      responsive: {
+        0: {
+          items: 1
+        },
+        500: {
+          items: 2
+        },
+        1200: {
+          items: 3
+        }
+      }
+    });
+  });
+
+  jQuery(document).ready(function($) {
+    "use strict";
+    $('#customers-testimonials2').owlCarousel( {
+        loop: false,
+        center: true,
+        items: 1,
+        margin: 50,
+        autoplay: false,
+        dots:true,
+        nav:true,
+        autoplayTimeout: 8500,
+        smartSpeed: 1200,
+        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+
+      });
+    });
+
+
+
+// #######################################################
+// ##################ANIMAATE ON SCROLL ##################
+// #######################################################
+AOS.init({
+  duration: 1200,
+  easing: 'ease-in-out-back'
+});
+
+// https://github.com/tsuyoshiwada/sweet-scroll/blob/master/README.md#1-install
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    const scroller = new SweetScroll({
+      /* some options */
+    });
+  },
+  false,
+);
+
+
+
+// #######################################################
+// ################## Back button #########################
+// #######################################################
+
+var backButton = document.querySelector('.back-button')
+
+function backAnim(){
+  if (backButton.classList.contains('back')){
+    backButton.classList.remove('back');
+  }
+  else{
+    backButton.classList.add('back');
+    setTimeout(backAnim, 1000);
+  }
+}
+backButton.addEventListener('click', backAnim);
+
+
+
+
+
+
+
+
+
+
