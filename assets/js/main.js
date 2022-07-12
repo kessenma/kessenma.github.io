be6eedac958e592f1bd408c11f475afc58238f26
Kyle Essenmacher <kessen@umich.edu>
2022-07-12 19:14:39 -0400

    changed aesthetics

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,71 @@
 // #######################################################
 // ##################ANIMATE ON SCROLL ##################
 // #######################################################
-// AOS.init({
-//   duration: 1200,
-//   easing: 'ease-in-out-back'
-// });
+AOS.init({
+  duration: 1200,
+  easing: 'ease-in-out-back'
+});
+
+// #######################################################
+// ################## typewrite  #########################
+// #######################################################
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
 
 
 // #######################################################
@@ -16,14 +77,14 @@
 document.addEventListener("DOMContentLoaded",function(){new SweetScroll({});particlesJS("particles-js",{
   "particles": {
     "number": {
-      "value": 10,
+      "value": 80,
       "density": {
         "enable": true,
         "value_area": 800
       }
     },
     "color": {
-      "value": "#ffffff"
+      "value": "#000000"
     },
     "shape": {
       "type": "circle",
@@ -41,8 +102,8 @@ document.addEventListener("DOMContentLoaded",function(){new SweetScroll({});part
       }
     },
     "opacity": {
-      "value": 0.6,
-      "random": true,
+      "value": 0.5,
+      "random": false,
       "anim": {
         "enable": false,
         "speed": 1,
@@ -51,8 +112,8 @@ document.addEventListener("DOMContentLoaded",function(){new SweetScroll({});part
       }
     },
     "size": {
-      "value": 28.05962670675185,
-      "random": false,
+      "value": 3,
+      "random": true,
       "anim": {
         "enable": false,
         "speed": 40,
@@ -61,52 +122,52 @@ document.addEventListener("DOMContentLoaded",function(){new SweetScroll({});part
       }
     },
     "line_linked": {
-      "enable": false,
+      "enable": true,
       "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
+      "color": "#000000",
+      "opacity": 1,
       "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 1.60340724038582,
-      "direction": "bottom",
+      "speed": 6,
+      "direction": "none",
       "random": false,
-      "straight": true,
+      "straight": false,
       "out_mode": "out",
       "bounce": false,
       "attract": {
-        "enable": true,
-        "rotateX": 400.851810096455,
+        "enable": false,
+        "rotateX": 600,
         "rotateY": 1200
       }
     }
   },
   "interactivity": {
-    "detect_on": "window",
+    "detect_on": "canvas",
     "events": {
       "onhover": {
         "enable": true,
-        "mode": "bubble"
+        "mode": "repulse"
       },
       "onclick": {
         "enable": true,
-        "mode": "bubble"
+        "mode": "push"
       },
       "resize": true
     },
     "modes": {
       "grab": {
-        "distance": 0,
+        "distance": 400,
         "line_linked": {
-          "opacity": 0
+          "opacity": 1
         }
       },
       "bubble": {
         "distance": 400,
         "size": 40,
-        "duration": 1.118881118881119,
-        "opacity": 0.3516483516483517,
+        "duration": 2,
+        "opacity": 8,
         "speed": 3
       },
       "repulse": {
